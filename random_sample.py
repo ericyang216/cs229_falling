f32d7630d329d1364f145ce1cdec8da362761df8
eyyang <eyyang@umich.edu>
2018-11-19 13:21:10 -0800

    smoothing failures

--- random_sample.py
+++ random_sample.py
@@ -1,15 +1,13 @@
 import random
 import os
 import math
+import json
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from glob import glob
-
-data_path = "data/2015"
-fail_path = '%s_failures.csv' % os.path.basename(data_path)
-out_path = '%s_sample.csv' % os.path.basename(data_path)
+from datetime import datetime, timedelta
 
 def rand_undersample(args):
     samp = pd.DataFrame()
@@ -18,21 +16,37 @@ def rand_undersample(args):
 
     files = glob(os.path.join(args.path, '*.csv'))
 
+    if args.json:
+        with open(args.json, 'r') as f:
+            fail_date_dict = json.load(f)
+        print("Loaded {}".format(args.json))
+
+
     # Oversample per file
     samp_per_file = math.ceil(num_fails/len(files)) * 2
     i = 0
-    
+
     for file in files:
-        try:
-            df = pd.read_csv(file)
-            s = df[df['failure'] == 0].sample(samp_per_file)
-            i = i + samp_per_file
-            print("[{}/{}] nominal sample from {}".format(i, num_fails, file))
-            samp = samp.append(s)
-        except KeyboardInterrupt:
-            break
-        except:
-            continue
+        # try:
+        df = pd.read_csv(file)
+        curr_date = df['date'][1]
+
+        fail_sns = []
+        for d in range(args.nday + 1):
+            next_datetime = datetime.strptime(curr_date, '%Y-%m-%d') + timedelta(days=d)
+            next_day = next_datetime.strftime('%Y-%m-%d')
+            if next_day in fail_date_dict:
+                fail_sns = fail_sns + fail_date_dict[next_day]
+
+        s = df[~df['serial_number'].isin(fail_sns)].sample(samp_per_file)
+        i = i + samp_per_file
+        # print(s['failure'])
+        print("[{}/{}] nominal sample from {}".format(i, num_fails, file))
+        samp = samp.append(s)
+        # except KeyboardInterrupt:
+        #     break
+        # except:
+        #     continue
 
     # Resample to exact amount
     samp = samp.sample(num_fails)
@@ -42,9 +56,11 @@ def rand_undersample(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create training csv by combining failures and sampling nominal data')
-    parser.add_argument('-f', '--fail', default=fail_path, help='fail csv')
-    parser.add_argument('-p', '--path', default=data_path, help='path to data directory')
-    parser.add_argument('-o', '--out', default=out_path, help='output file name (xxx.csv)')
+    parser.add_argument('-f', '--fail', required=True, help='fail csv')
+    parser.add_argument('-p', '--path', required=True, help='path to data directory')
+    parser.add_argument('-o', '--out', required=True, help='output file name (xxx.csv)')
+    parser.add_argument('-j', '--json', required=True, help='failure sn dates json')
+    parser.add_argument('-n', '--nday', required=True, type=int, help='smooth additional n day as failure')
 
     args = parser.parse_args()
     rand_undersample(args)
--- save_failures.py
+++ save_failures.py
@@ -1,40 +1,81 @@
 import os
 import argparse
+import json
 import pandas as pd
 from glob import glob
+from datetime import datetime, timedelta
 
-data_path = "data/2015"
-out_path = '%s_failures.csv' % os.path.basename(data_path)
+pd.options.mode.chained_assignment = None
+
 
 def save_failures(args):
-    all_fail = pd.DataFrame()
-
-    i = 1
-    data_size = 0
-    fail_size = 0
-
     files = glob(os.path.join(args.path, "*.csv"))
-    for file in files:
-        all = pd.read_csv(file)
-        fails = all[all['failure'] == 1]
-        fails = fails[fails['capacity_bytes'] > 0]
 
-        print("[{}/{}] {} failures from {}".format(i, len(files), len(fails.index), file))
+    fail_date_dict = {}
 
-        all_fail = all_fail.append(fails)
+    if args.json:
+        with open(args.json, 'r') as f:
+            fail_date_dict = json.load(f)
+        print("Loaded {}".format(args.json))
 
-        data_size = data_size + len(all.index)
-        fail_size = fail_size + len(fails.index)
-        i = i + 1
+    else:
+        for i, file in enumerate(files):
+            all = pd.read_csv(file)
+            fails = all[all['failure'] == 1]
+            fails = fails[fails['capacity_bytes'] > 0]
 
-    print("{} failures / {} total samples".format(fail_size, data_size))
-    print("Saving failures to {}".format(args.out))
+            date = all['date'][1]
+            fail_date_dict[date] = list(fails['serial_number'])
+
+            print('[{}/{}] {} failing serial numbers in {}'\
+                  .format(i, len(files), len(list(fails['serial_number'])), file))
+
+        print("Saving sn_date_failures.json")
+        with open('sn_date_failures.json', 'w') as outfile:
+            json.dump(fail_date_dict, outfile, indent=4, sort_keys=True,
+                          separators=(',', ': '), ensure_ascii=False)
+
+    all_fail = pd.DataFrame()
+    num_fail = 0
+
+    for date_key in fail_date_dict:
+
+        print("Backtracking {} day(s) for date {} ({} failures)"\
+              .format(args.nday, date_key, len(fail_date_dict[date_key])))
+
+        fail_dates = []
+        for i in range(args.nday + 1):
+            date = datetime.strptime(date_key, '%Y-%m-%d') - timedelta(days=i)
+            fail_dates.append(date.strftime('%Y-%m-%d'))
+
+        for date in fail_dates:
+            # print("{} - finding failing serial numbers".format(date))
+            file = os.path.join(args.path, date + ".csv")
+
+            try:
+                all = pd.read_csv(file)
+
+                sns = fail_date_dict[date_key]
+                fails = all[all['serial_number'].isin(sns)]
+
+                if not fails.empty:
+                    fails.loc[:, 'failure'] = 1
+                    all_fail = all_fail.append(fails)
+                    num_fail = num_fail + len(fails)
+
+            except KeyboardInterrupt:
+                exit()
+            except:
+                continue
+
+    print("Saving {} failures to {}".format(num_fail, args.out))
     all_fail.to_csv(args.out, index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse all data and save all failures to single csv')
-    parser.add_argument('-p', '--path', default=data_path, help='path to data directory')
-    parser.add_argument('-o', '--out', default=out_path, help='output file name (xxx.csv)')
-
+    parser.add_argument('-p', '--path', required=True, help='path to data directory')
+    parser.add_argument('-o', '--out', required=True, help='output file name (xxx.csv)')
+    parser.add_argument('-n', '--nday', required=True, type=int, help='smooth additional n day as failure')
+    parser.add_argument('-j', '--json', required=False, default=None, help='failure sn dates json')
     args = parser.parse_args()
     save_failures(args)
